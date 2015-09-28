9e8c69c16de2c863a8a07e47f844d1d6fdca8551
jaffemd <jaffemd0125@gmail.com>
2015-09-28 13:20:37 -0500

    tags-edit

--- app/example/scripts/CommentsController.js
+++ app/example/scripts/CommentsController.js
@@ -2,18 +2,13 @@ angular
   .module('example')
   .controller('CommentsController', function($scope, supersonic) {
               $scope.tags = [
-                             { text: 'smart' },
-                             { text: 'funky' },
-                             { text: 'cool' },
-                             { text: 'dapper' },
-                             { text: 'suave' },
-                             { text: 'sloppy' },
-                             { text: 'unfashionable' },
-                             { text: 'sexy' },
-                             { text: 'bold' },
-                             { text: 'hot' },
-                             { text: 'old-fashioned' },
-                             { text: 'loose' },
+                             { text: 'perfect' },
+                             { text: 'almost there' },
+                             { text: 'start over from scratch' },
+                             { text: 'overdressed' },
+                             { text: 'underdressed' },
+                             { text: 'good color choice' },
+                             { text: 'colors don\'t match' },
                              ];
               
               $scope.submitComments = function()
