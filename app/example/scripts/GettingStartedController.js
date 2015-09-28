62a4e078ed750860eaeac8dd42d2a099d3074d81
Anuj Iravane <anudge7@gmail.com>
2015-09-28 10:53:44 -0500

    accessing data from supersonic database (broken)

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -1,7 +1,14 @@
 angular
   .module('example')
   .controller('GettingStartedController', function($scope, supersonic) {
-    
+    var PictureData = supersonic.data.model('PictureData');
+     var images ;
+    var index = 0;
+    PictureData.findAll().then(function (allPictures) {
+	$scope.images = allPictures;
+    });
+    $scope.currentImage = $scope.images[index].imageUrl;
+    //console.log(images[0].imageUrl);
     var options = {
                     message: "Leave your feedback",
                     buttonLabel: "Close"
@@ -12,6 +19,8 @@ angular
 			var options = {
  			 animate: true
 				          }
+			index += 1;
+			$scope.currentImage = images[index%images.length].imageUrl;
 			supersonic.ui.modal.show(modalView, options);
 			}
 	
