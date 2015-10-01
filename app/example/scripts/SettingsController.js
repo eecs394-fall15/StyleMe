02733b05c85f85aa98f73ec85cd97e255b94850d
Sam Cohen <samc1213@gmail.com>
2015-10-01 14:02:29 -0500

    coke can uploaded

--- app/example/scripts/SettingsController.js
+++ app/example/scripts/SettingsController.js
@@ -2,14 +2,30 @@ angular
   .module('example')
   .controller('SettingsController', function($scope, supersonic, $firebaseObject) {
     $scope.navbarTitle = "Settings";
-	var ref = new Firebase("https://styleme1.firebaseio.com/");
+	
+
+	var options = {
+	  quality: 50,
+	  allowEdit: true,
+	  targetWidth: 300,
+	  targetHeight: 300,
+	  encodingType: "png",
+	  saveToPhotoAlbum: true,
+	  destinationType: "dataURL"
+	};
+	supersonic.media.camera.takePicture(options).then( function(result){
+		$scope.base64 = result;
+		var ref = new Firebase("https://styleme1.firebaseio.com/");
+		// var syncObject = $firebaseObject(ref);
+		ref.set(result);
+  		// syncObject.set({pic: $scope.base64});
+	});
+
 
   	// download the data into a local object
-  	$scope.data = $firebaseObject(ref);
+  	// $scope.data = $firebaseObject(ref);
 
-  	var syncObject = $firebaseObject(ref);
-
-  	syncObject.$bindTo($scope, "data");
+  	
 
   // putting a console.log here won't work, see below
 });
