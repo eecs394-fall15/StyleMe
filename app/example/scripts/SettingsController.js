0a5c63f480cccdc65cbe7dcf5350251057434db8
jaffemd <jaffemd0125@gmail.com>
2015-10-01 21:06:51 -0500

    added take picture button in settings view

--- app/example/scripts/SettingsController.js
+++ app/example/scripts/SettingsController.js
@@ -3,27 +3,27 @@ angular
   .controller('SettingsController', function($scope, supersonic, $firebaseObject) {
     $scope.navbarTitle = "Settings";
 	
-
-	var options = {
-	  quality: 50,
-	  allowEdit: true,
-	  targetWidth: 300,
-	  targetHeight: 300,
-	  encodingType: "png",
-	  saveToPhotoAlbum: true,
-	  destinationType: "dataURL"
-	};
-	supersonic.media.camera.takePicture(options).then( function(result){
-		$scope.base64 = result;
-		var ref = new Firebase("https://styleme1.firebaseio.com/");
-		// var syncObject = $firebaseObject(ref);
-		ref.push({
-		  title: "Appropriate for an interview?",
-		  image: result
+    $scope.openCamera = function(){
+		var options = {
+		  quality: 50,
+		  allowEdit: true,
+		  targetWidth: 300,
+		  targetHeight: 300,
+		  encodingType: "png",
+		  saveToPhotoAlbum: true,
+		  destinationType: "dataURL"
+		};
+		supersonic.media.camera.takePicture(options).then( function(result){
+			$scope.base64 = result;
+			var ref = new Firebase("https://styleme1.firebaseio.com/");
+			// var syncObject = $firebaseObject(ref);
+			ref.push({
+			  title: "Appropriate for an interview?",
+			  image: result
+			});
+	  		// syncObject.set({pic: $scope.base64});
 		});
-  		// syncObject.set({pic: $scope.base64});
-	});
-
+	};
 
   	// download the data into a local object
   	// $scope.data = $firebaseObject(ref);
