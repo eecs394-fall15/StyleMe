9797c42b3c1ab443aa2fd518d3747ef1f8deb99d
Sam Cohen <samc1213@gmail.com>
2015-10-05 10:52:45 -0500

    added titles

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -3,26 +3,32 @@ angular
   .controller('GettingStartedController', ['$scope', 'supersonic', 'incrementImageIndex', function($scope, supersonic, incrementImageIndex, $firebaseObject) {
     var ref = new Firebase("https://styleme1.firebaseio.com/");
     var images = [];
+    var titles = [];
     ref.once("value", function(data) {
       for (var key in data.val()){
         supersonic.logger.log(key);
         if (data.val().hasOwnProperty(key)){
           images.push(data.val()[key].image);
+          titles.push(data.val()[key].title);
         }
       }
           $scope.currentImage = images[incrementImageIndex.getCount()%images.length];
+          $scope.currentTitle = titles[incrementImageIndex.getCount()%images.length];
           //supersonic.logger.log(images[0]);
           $scope.$apply; 
   // do some stuff once
     });
     ref.on("value", function(snapshot) {
       images = [];
+      titles = [];
       for (var key in snapshot.val()){
         if (snapshot.val().hasOwnProperty(key)){
           images.push(snapshot.val()[key].image);
+          titles.push(snapshot.val()[key].title);
         }
       }
     $scope.currentImage = images[incrementImageIndex.getCount()%images.length];
+    $scope.currentTitle = titles[incrementImageIndex.getCount()%images.length];
   console.log(snapshot.val());
     }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
@@ -58,6 +64,7 @@ angular
 			var index = incrementImageIndex.incrementCount();
 			console.log(index);
 			$scope.currentImage = images[index%images.length];
+      $scope.currentTitle = titles[index%titles.length];
 			console.log($scope.currentImage);
 			//$scope.$apply();
 			supersonic.ui.modal.show(modalView, options);
