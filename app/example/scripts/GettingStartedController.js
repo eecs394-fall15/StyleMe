6bfe765e84fc5a789e5001dd44aa7950a98dcefb
Abhishek Raj <abhishekraj2016@u.northwestern.edu>
2015-10-21 17:26:07 -0500

    Refresh for Home page

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -4,12 +4,14 @@ angular
   function($scope, supersonic) {
     $scope.isLoading = true;
     $scope.Images = [];
+    $scope.noMoreImages =false;
     var imageindex = 0;
-    $scope.currentImg = "BOOBIES";
+    $scope.currentImg = "YO";
     var CustClass = Parse.Object.extend("newimg");
 var query = new Parse.Query(CustClass);
 query.find({
   success: function(results) {
+           supersonic.logger.log("Sucess");
     // Do something with the returned Parse.Object values
     for (var i = 0; i < results.length; i++) {
       var object = results[i];
@@ -33,22 +35,6 @@ query.find({
      //         $scope.isLoading = false;
      //         $scope.cardIndex = 0;
      //         });
-             
-     // var incrementImageIndex = function(){
-             
-     //         if(incrementIndex.getCount() < $scope.swipeArray.length - 1)
-     //            $scope.cardIndex = incrementIndex.incrementCount()%$scope.swipeArray.length;
-     //         else {
-     //                $scope.noMoreImages = true;
-     //                var options = {
-     //                                message: "No more images for now! :(",
-     //                                buttonLabel: "Close"
-     //                };
-     //                supersonic.ui.dialog.alert("Oops!", options).then(function() {
-     //                                supersonic.logger.log("Alert closed.");
-     //                                });
-     //              }
-     // }
      
      // var animate = function(action){
      //      var options = {duration: .4}
@@ -60,44 +46,87 @@ query.find({
           var image = $scope.Images[imageindex % $scope.Images.length];
           image.increment("dislikes");
           image.save();
+          if(imageindex < $scope.Images.length - 1){
           imageindex = imageindex + 1;
+           supersonic.logger.log("Image Index:" + imageindex);
+           supersonic.logger.log("Images Length" + $scope.Images.length);
           var obj = $scope.Images[imageindex % $scope.Images.length];
           var img = obj.get('newimage');
           $scope.currentImg = img.url();
           $scope.currentTitle = obj.get('title');
-
+          //Animate                       
           var options = {duration:.4}
           supersonic.ui.animate("slideFromRight",options).perform();
+          }
+        else{
+              $scope.noMoreImages = true;
+              var options = {
+                     message: "No more images for now! :(",
+                     buttonLabel: "Close"
+                    };
+              supersonic.ui.dialog.alert("Oops!", options).then(function() {
+                 supersonic.logger.log("Alert closed.");
+               });
+        }
      }
                                          
      $scope.swipeRight = function(){
           var image = $scope.Images[imageindex % $scope.Images.length];
           image.increment("likes");
           image.save();
+          if(imageindex < $scope.Images.length - 1){
+           supersonic.logger.log("Image Index:" + imageindex);
+           supersonic.logger.log("Images Length" + $scope.Images.length);
           imageindex = imageindex + 1;
           var obj = $scope.Images[imageindex % $scope.Images.length];
           var img = obj.get('newimage');
           $scope.currentImg = img.url();
           $scope.currentTitle = obj.get('title');
-
+          //Animate
           var options = {duration:.4}
           supersonic.ui.animate("slideFromLeft",options).perform();
+                                         }
+         else{
+                $scope.noMoreImages = true;
+                var options = {
+                     message: "No more images for now! :(",
+                     buttonLabel: "Close"
+                  };
+              supersonic.ui.dialog.alert("Oops!", options).then(function() {
+                  supersonic.logger.log("Alert closed.");
+              });
+        }
      }
                                          
-     // $scope.refreshFeed = function() {
-        
-     //    if(incrementIndex.getCount() < $scope.swipeArray.length - 1){
-     //         $scope.noMoreImages = false;
-     //         $scope.cardIndex = incrementIndex.incrementCount()%$scope.swipeArray.length;
-     //    }
-     //    else {
-     //        var options = {
-     //                        message: "Try after sometime!",
-     //                        buttonLabel: "Close"
-     //                       };
-     //        supersonic.ui.dialog.alert("Sorry!", options).then(function() {
-     //                supersonic.logger.log("Alert closed.");
-     //        });
-     //    }
-     //  }
+      $scope.refreshFeed = function() {
+            query.find({
+                success: function(results) {
+                supersonic.logger.log("Sucess");
+                // Do something with the returned Parse.Object values
+                if(imageindex < results.length - 1){
+                 $scope.noMoreImages = false;
+                 imageindex = imageindex + 1;
+                  for (var i = imageindex; i < results.length; i++) {
+                       var object = results[i];
+                       $scope.Images.push(object);
+                        }
+                        var obj = $scope.Images[imageindex];
+                        var img = obj.get('newimage');
+                        $scope.currentImg = img.url();
+                        $scope.currentTitle = obj.get('title');  
+                       }
+                  else{ var options = {
+                          message: "Try after sometime!",
+                          buttonLabel: "Close"
+                         };
+                        supersonic.ui.dialog.alert("Sorry!", options).then(function() {
+                           supersonic.logger.log("Alert closed.");
+                            });
+                       }
+                    },
+                  error: function(error) {
+                  supersonic.ui.dialog.alert('ABANDON SHIP!!');
+                }
+          });   
+     }
      }]);
