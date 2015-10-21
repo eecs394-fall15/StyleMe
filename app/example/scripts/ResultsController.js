e92fa252fca55b826b1c005d7f98cd0d6c010422
jaffemd <jaffemd0125@gmail.com>
2015-10-21 13:51:11 -0500

    added a refresh button

--- app/example/scripts/ResultsController.js
+++ app/example/scripts/ResultsController.js
@@ -8,6 +8,7 @@ angular
     var CustClass = Parse.Object.extend("newimg");
     var query = new Parse.Query(CustClass);
     query.equalTo("userid", authData.uid);
+
     query.find({
       success: function(results) {
         // supersonic.ui.dialog.alert(results.length);
@@ -15,8 +16,7 @@ angular
         for (var i = 0; i < results.length; i++) {
           var object = results[i];
           var newResImg = {};
-          // supersonic.logger.log(object.get("likes"));
-          // $scope.likeylike = object.get('likes');
+
           newResImg.likes = object.get("likes");
           newResImg.dislikes = object.get("dislikes");
           newResImg.title = object.get("title");
@@ -29,29 +29,32 @@ angular
         supersonic.ui.dialog.alert('ABANDON SHIP!!');
       }
     });
-    // $scope.fetchImageById = function(){
-    //   $scope.searchID = $scope.userid;
-    // };
-  	//var authData = null;
-  	//$scope.test = "hey";
-  	//$scope.$apply;
-  	// var ref = new Firebase("https://styleme1.firebaseio.com/images");
-  	// $scope.data = [];
-  	// if (authData){
-  	// 	supersonic.logger.log("Hey we have the user");
-  	// 	$scope.test = "got here";
-  	// 	ref("once", function(data){
-  	// 		for (var key in data.val){
-			// 	if (data.val().hasOwnProperty(key)){
-			// 		if (data.val()[key].userid == authData.uid){
-			// 			$scope.data.push(data.val()[key].likes);
-			// 		}
-  	// 		}
-  	// 	}
-  	// 	$scope.$apply;
-  	// });
-  	// } else {
-  	// 	supersonic.logger.log("it was null");
-  	// }
+
+    $scope.refresh = function(){
+      $scope.resultImages = [];
+      query.find({
+        success: function(results) {
+          supersonic.logger.log("running refresh");
+          // supersonic.ui.dialog.alert(results.length);
+          // Do something with the returned Parse.Object values
+          for (var i = 0; i < results.length; i++) {
+            var object = results[i];
+            var newResImg = {};
+
+            newResImg.likes = object.get("likes");
+            newResImg.dislikes = object.get("dislikes");
+            newResImg.title = object.get("title");
+            var img = object.get('newimage');
+            newResImg.photo = img.url();
+            $scope.resultImages.push(newResImg);
+          }
+        },
+        error: function(error) {
+          supersonic.ui.dialog.alert('ABANDON SHIP!!');
+        }
+      });
+      $scope.$apply;
+    }
+
   }]);
 
